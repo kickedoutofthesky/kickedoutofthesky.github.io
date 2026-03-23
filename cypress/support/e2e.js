b75b7dd5a72240d32003c4d99b06dfa07384e67a
Matt Bachman <kickedoutofthesky@gmail.com>
2026-03-23 19:50:45 +0000

    Move helpers to e2e.js support file for global availability in tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -13,8 +13,55 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import shared test helpers
-import "./helpers.js";
+/* global cy */
+/* eslint-disable no-undef */
+
+// Shared test helpers for all Cypress tests
+
+// eslint-disable-next-line no-unused-vars
+function selectFirstRealSize() {
+  // Wait for size options to be populated (not just the placeholder)
+  cy.get("[data-testid='size-select'] option").should("have.length.greaterThan", 1);
+
+  cy.get("[data-testid='size-select']").then($select => {
+    const value = $select.val();
+    // If placeholder is selected (empty value), select first real option
+    if (!value || value === "") {
+      cy.get("[data-testid='size-select'] option")
+        .eq(1)
+        .invoke("attr", "value")
+        .then(sizeValue => {
+          cy.get("[data-testid='size-select']").select(sizeValue);
+        });
+    }
+  });
+}
+
+// eslint-disable-next-line no-unused-vars
+function selectFirstRealColor() {
+  // Wait for color select to be visible and have options
+  cy.get("[data-testid='color-select']").should("exist");
+  cy.get("[data-testid='color-select'] option").should("have.length.greaterThan", 0);
+
+  cy.get("[data-testid='color-select']").then($select => {
+    if ($select.length > 0 && $select.find("option").length > 0) {
+      cy.get("[data-testid='color-select'] option")
+        .eq(0) // Get the first color (no placeholder in color select)
+        .invoke("attr", "value")
+        .then(colorValue => {
+          // Use force:true to bypass navbar coverage issue
+          cy.get("[data-testid='color-select']").select(colorValue, { force: true });
+          // Wait for color change to update sizes
+          cy.get("[data-testid='size-select'] option").should("have.length.greaterThan", 1);
+        });
+    }
+  });
+}
+
+// eslint-disable-next-line no-unused-vars
+function selectShippingCountry(countryCode = "US") {
+  cy.get("#shipping-country").should("exist").select(countryCode);
+}
 
 // Ignore cross-origin script errors from CDN resources (Bootstrap, Font Awesome)
 // These are expected and don't affect test functionality
